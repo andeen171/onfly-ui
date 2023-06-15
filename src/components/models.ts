df12a2e6bffc501c21fb38a795903fa95cf0352a
Anderson Lopes <zeander21@outlook.com.br>
2023-06-15 19:57:33 -0300

    Index page overraul for crud table

--- src/components/models.ts
+++ src/components/models.ts
@@ -6,3 +6,11 @@ export interface Todo {
 export interface Meta {
   totalCount: number;
 }
+
+export interface Expense {
+  id: number;
+  description: string;
+  date: string;
+  value: number;
+  user_id: number;
+}
